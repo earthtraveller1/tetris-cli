a8daab8a362666aa6fe81dec71bc8e7a69b120e5
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-02-28 16:30:35 -0500

    Did a sanity test. (It works).

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 mod screen;
 
+use screen::Screen;
+
 fn main() {
-    println!("Hello, world!");
+    let mut screen = Screen::new(12, 12).unwrap();
+    for i in 0..12 {
+        for j in 0..12 {
+            let pixel = &mut screen[i][j as usize];
+            
+            pixel.set_shape('$', '$');
+            
+            if i == j {
+                pixel.set_color(screen::Color::RGB(255, 0, 255));
+            }
+        }
+    }
+    screen.present();
 }
