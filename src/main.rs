5f933285f681fe5615fa5d2c5eb4e501055febdd
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-03-14 12:05:27 -0400

    Refractoring + Integrated the shape renderer.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod screen;
 mod system;
 
 use screen::Screen;
+use screen::Shape;
 
 fn main() {
     let mut screen = Screen::new(16, 16).unwrap();
@@ -9,33 +10,39 @@ fn main() {
     let player_x = 8;
     let mut player_y = 8;
 
-    screen[player_x][player_y].set_shape('#', '#');
+    let player_shape = Shape {
+        pixels: vec![(0, 0), (0, 1), (0, -1), (-1, 0), (1, 0)],
+        fill_pixel: screen::Pixel {
+            shape: ['#', '#'],
+            color: screen::Color::Basic(screen::colors::basic::GREEN),
+        },
+    };
+
+    screen.draw_shape(&player_shape, player_x, player_y);
     screen.present();
 
     let mut running = true;
-    
-    let mut color = screen::colors::basic::RED;
-    
     while running {
         if let Some(input) = Screen::read_input() {
             match input {
-                'w' => if player_y < (screen.width() - 1).try_into().unwrap() { player_y += 1 },
-                's' => if player_y > 0 { player_y -= 1 },
+                'w' => {
+                    if player_y < (screen.width() - 1).try_into().unwrap() {
+                        player_y += 1
+                    }
+                }
+                's' => {
+                    if player_y > 0 {
+                        player_y -= 1
+                    }
+                }
                 'q' => running = false,
                 _ => (),
             }
         }
-        
-        if color == 37 {
-            color = 30;
-        } else {
-            color += 1;
-        }
 
         screen.clear();
+        screen.draw_shape(&player_shape, player_x, player_y);
 
-        screen[player_x][player_y].set_shape('#', '#');
-        screen[player_x][player_y].set_color(screen::Color::Basic(color));
         screen.present();
     }
 }
