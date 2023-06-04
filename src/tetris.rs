2f8c473be27ed101fa1191a8874b812cf214e2d8
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-06-04 14:03:11 -0400

    Wrote a basic Tetris class.

--- src/tetris.rs
+++ src/tetris.rs
@@ -1,4 +1,43 @@
 // This file contains all the logic that is related to the actual Tetris game itself.
 // This includes the game mechanics, the game abstractions, etc.
 
-// TODO: Put something instead of just comments here.
+use std::num::TryFromIntError;
+
+use crate::screen::{self, OutOfBoundsError, Pixel, Screen};
+
+const SCREEN_WIDTH: u32 = 10;
+const SCREEN_HEIGHT: u32 = 20;
+
+pub struct Tetris {
+    screen: Screen,
+    running: bool,
+}
+
+impl Tetris {
+    pub fn new() -> Result<Tetris, TryFromIntError> {
+        Ok(Tetris {
+            screen: Screen::new(SCREEN_WIDTH, SCREEN_HEIGHT)?,
+            running: true,
+        })
+    }
+
+    pub fn update(&mut self) {
+        if let Ok(input) = self.screen.read_input() {
+            // TODO: Do stuff here
+        }
+    }
+
+    pub fn render(&mut self) -> Result<(), OutOfBoundsError> {
+        self.screen.clear();
+        self.screen.fill_with_pixel(&Pixel {
+            shape: [crate::unicode::LIGHT_SHADE, ' '],
+            color: screen::Color::Basic(screen::colors::basic::BRIGHT_BLACK),
+        });
+        self.screen
+            .draw_box(0, 0, (SCREEN_WIDTH - 2) as u16, (SCREEN_HEIGHT - 2) as u16)?;
+
+        self.screen.present();
+
+        Ok(())
+    }
+}
