b565fcd31d08eb983dc3eae04e977b83ffa96acb
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-06-04 15:22:49 -0400

    Added shapes.

--- src/tetris.rs
+++ src/tetris.rs
@@ -3,14 +3,37 @@
 
 use std::num::TryFromIntError;
 
-use crate::screen::{self, OutOfBoundsError, Pixel, Screen};
+use crate::screen::{self, OutOfBoundsError, Pixel, Screen, Shape};
 
 const SCREEN_WIDTH: u32 = 10;
 const SCREEN_HEIGHT: u32 = 20;
 
+struct Shapes {
+    square: Shape,
+}
+
+impl Shapes {
+    fn new() -> Shapes {
+        use crate::screen::{colors::basic::*, Color};
+        use crate::unicode::FULL_BLOCK;
+
+        Shapes {
+            square: Shape {
+                pixels: vec![(0, 0), (1, 0), (1, 1), (0, 1)],
+                fill_pixel: Pixel {
+                    shape: [FULL_BLOCK, FULL_BLOCK],
+                    color: Color::Basic(YELLOW),
+                },
+            },
+        }
+    }
+}
+
 pub struct Tetris {
     screen: Screen,
     is_running: bool,
+
+    shapes: Shapes,
 }
 
 impl Tetris {
@@ -18,6 +41,7 @@ impl Tetris {
         Ok(Tetris {
             screen: Screen::new(SCREEN_WIDTH + 2, SCREEN_HEIGHT + 2)?,
             is_running: true,
+            shapes: Shapes::new(),
         })
     }
 
@@ -43,6 +67,8 @@ impl Tetris {
 
         self.screen
             .draw_box(0, 0, (SCREEN_WIDTH + 1) as u16, (SCREEN_HEIGHT + 1) as u16)?;
+
+        self.screen.draw_shape(&self.shapes.square, 5, 5);
 
         self.screen.present();
 
