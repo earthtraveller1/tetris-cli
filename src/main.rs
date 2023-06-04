aebf3cc2cd10c24dac10322dde53b3ec90f1e98f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-06-04 14:16:24 -0400

    Used the tetris class instead of the old game class.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ mod screen;
 mod system;
 mod tetris;
 
-use screen::Screen;
-use screen::Shape;
-
 use std::thread;
 use std::time::Duration;
 use std::time::Instant;
@@ -25,78 +22,8 @@ mod unicode {
     pub const BOX_DRAWINGS_LIGHT_UP_AND_LEFT: char = '\u{2518}';
 }
 
-struct Game {
-    screen: Screen,
-    running: bool,
-
-    player_shape: Shape,
-
-    player_x: u16,
-    player_y: u16,
-}
-
-impl Game {
-    fn new() -> Game {
-        let screen = Screen::new(16, 16).unwrap();
-
-        Game {
-            screen,
-            player_shape: Shape {
-                pixels: vec![(0, 0), (0, 1), (0, -1), (-1, 0), (1, 0)],
-                fill_pixel: screen::Pixel {
-                    shape: [unicode::FULL_BLOCK, unicode::FULL_BLOCK],
-                    color: screen::Color::Basic(screen::colors::basic::GREEN),
-                },
-            },
-            running: true,
-            player_x: 8,
-            player_y: 8,
-        }
-    }
-
-    fn update(&mut self) {
-        if let Ok(input) = self.screen.read_input() {
-            match input {
-                'w' => {
-                    if self.player_y < (self.screen.width() - 1).try_into().unwrap() {
-                        self.player_y += 1
-                    }
-                }
-                's' => {
-                    if self.player_y > 0 {
-                        self.player_y -= 1
-                    }
-                }
-                'q' => self.running = false,
-                _ => (),
-            }
-        }
-    }
-
-    fn render(&mut self) {
-        self.screen.clear();
-        self.screen.fill_with_pixel(&screen::Pixel {
-            shape: [unicode::LIGHT_SHADE, ' '],
-            color: screen::Color::Basic(screen::colors::basic::BRIGHT_BLACK),
-        });
-        self.screen.draw_box(0, 0, 8, 8).unwrap();
-        self.screen
-            .draw_shape(&self.player_shape, self.player_x, self.player_y);
-
-        self.screen.present();
-    }
-
-    fn is_running(&self) -> bool {
-        self.running
-    }
-}
-
 fn main() {
-    let mut game = Game::new();
-
-    // Render the first frame so that the user isn't staring at a blank
-    // screen on startup.
-    game.render();
+    let mut game = tetris::Tetris::new().expect("Uh oh");
 
     while game.is_running() {
         let start = Instant::now();
