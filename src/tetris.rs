5bdab4fd22d97f0b35b9cd5b467a2f38a842ae27
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-06-17 16:23:40 -0400

    Revert "Revert "Fixed blocks cutting into each other.""
    This reverts commit b0403b46b6ec300d246d7b316605b7178edcf02c.

--- src/tetris.rs
+++ src/tetris.rs
@@ -180,8 +180,7 @@ impl Tetris {
 
                 // Check that it is not colliding with fossilized blocks.
 
-                if let Some(_) = self.blocks
-                    [<i16 as TryInto<usize>>::try_into(block_y - 1).unwrap()]
+                if let Some(_) = self.blocks[<i16 as TryInto<usize>>::try_into(block_y).unwrap()]
                     [<i16 as TryInto<usize>>::try_into(block_x - 1).unwrap()]
                 {
                     within_x_bounds = false;
